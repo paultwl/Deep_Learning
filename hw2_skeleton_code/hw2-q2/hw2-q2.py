9d14d9564c1c2cd277218fca96858cb96fffd75c
ist1113103 <pauli-tapio.lahtinen@tecnico@ulisboa.pt>
2025-01-10 17:51:17 +0000

    Changed parameter calculation

--- hw2_skeleton_code/hw2-q2/hw2-q2.py
+++ hw2_skeleton_code/hw2-q2/hw2-q2.py
@@ -196,9 +196,10 @@ def get_number_trainable_params(model):
         list(model.mlp_layer_2.parameters()) +
         list(model.mlp_out.parameters())
     )
+    model.parameters()
     
     # Sum the number of elements for all trainable parameters
-    cnn_params_count = sum(p.numel() for p in params if p.requires_grad)
+    cnn_params_count = sum(p.numel() for p in model.parameters() if p.requires_grad)
     
     return cnn_params_count
 
